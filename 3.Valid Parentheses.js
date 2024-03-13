ffe030399797d59753765f6eb9b17f434934c63f
manansaipi <abdul.saipi@student.president.ac.id>
2024-03-13 14:14:21 +0700

    delete unecesarry code

--- 3.Valid Parentheses.js
+++ 3.Valid Parentheses.js
@@ -3,42 +3,6 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
-	let countOpenParentheses = 0;
-	let countOpenBrackets = 0;
-	let countOpenCurlyBraces = 0;
-
-	for (let char of s) {
-		if (char === "(") {
-			countOpenParentheses++;
-		} else if (char === "[") {
-			countOpenBrackets++;
-		} else if (char === "{") {
-			countOpenCurlyBraces++;
-		} else if (char === ")") {
-			if (countOpenParentheses === 0) {
-				return false;
-			}
-			countOpenParentheses--;
-		} else if (char === "]") {
-			if (countOpenBrackets === 0) {
-				return false;
-			}
-			countOpenBrackets--;
-		} else if (char === "}") {
-			if (countOpenCurlyBraces === 0) {
-				return false;
-			}
-			countOpenCurlyBraces--;
-		}
-	}
-
-	return (
-		countOpenParentheses === 0 &&
-		countOpenBrackets === 0 &&
-		countOpenCurlyBraces === 0
-	);
-};
 //use stackkk
 var isValidUsingStack = function (s) {
 	const stack = [];
