2c38df4e7a78504d84c60c7c4751028ccc2dfa77
manansaipi <abdul.saipi@student.president.ac.id>
2024-03-18 08:31:10 +0700

    day8

---
+++ 8. 1. Two Sum.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+	return nums;
+};
+console.log(twoSum([2, 7, 11, 15], 9));
---
+++ 8. 27. Remove Element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+	let i = 0;
+	let length = nums.length;
+	while (i < length) {
+		if (nums[i] === val) {
+			nums.splice(i, 1);
+			length--;
+		} else {
+			i++;
+		}
+	}
+	return nums.length;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
