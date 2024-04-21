9f0471677eb60eec74fc68f2a7218cc4ba35d4e5
manansaipi <boben.manan5@gmail.com>
2024-04-21 08:30:24 +0700

    day32

--- solutions/32. 153. Find Minimum in Rotated Sorted Array.js
+++ solutions/32. 153. Find Minimum in Rotated Sorted Array.js
@@ -5,17 +5,22 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let counter = 0  ;
-    let result;
+    let counter = 0;  // Counter to track if there is a decreasing sequence
+    let result;       // Variable to store the minimum element
+    
+    // Loop through the array to find the minimum element
     for (let index = 0; index < nums.length; index++) {
-        if (nums[index] > nums[index+1]) {
-            result = nums[index+1]
-            counter ++
+        // If the current element is greater than the next element
+        if (nums[index] > nums[index+1]) {  
+            // Update the result to the next element
+            result = nums[index+1];         
+            // Increment the counter to indicate a decreasing sequence
+            counter++;                      
         }
-        
     }
     
-    return counter > 0 ? result : nums[0]
+    // If there was a decreasing sequence, return the result, otherwise return the first element
+    return counter > 0 ? result : nums[0];
 };
 
 console.log(findMin([3,4,5,1,2]));
