725f757030b33b0aadab59beaa9021e545132cd0
manansaipi <boben.manan5@gmail.com>
2024-04-24 18:00:04 +0700

    day35

---
+++ solutions/35. 3120. Count the Number of Special Characters I.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/count-the-number-of-special-characters-i/description
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numberOfSpecialChars = function(word) {
+    a = 'abcdefghijklmnopqrstuvwxyz'.split('');
+};
+
+console.log(word);
